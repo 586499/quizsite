381fb2148d879f2d52c85895d8821282d1b2b5d8
586499 <somersliu0324@gmail.com>
2025-11-04 11:28:13 +0800

    make mvnw executable and update controller

--- src/main/java/com/quizsite/quiz/mathQuizController.java
+++ src/main/java/com/quizsite/quiz/mathQuizController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.quizsystem.utils.CsvUtil;
 
 
 import jakarta.servlet.http.HttpSession;
